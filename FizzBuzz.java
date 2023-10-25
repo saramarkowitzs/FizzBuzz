b83a7ce48163cb9f52c6ef08ade37b880faa7e01
Sara Markowitz <sara.markowitz@mrjaffesclass.com>
2023-10-25 11:20:35 -0700

    ad

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,27 +2,21 @@ public class FizzBuzz{
 
 public static void main(String[] args) {
         for (int i=1; i<=100; i++){
-            String output = FizzBuzz(i);
-            System.out.println(output);
-        }
-}
- public static String FizzBuzz(int n) {
-        int d3 = ( n - 1) / 3;
-        int d5 = ( n - 1) / 5;
-        int r3 = n - (d3*3);
-        int r5 = n - (d3*5);
-        
-        if (r3 == 0 && r5 ==0){
-            return "FizzBuzz";
-        }else{
-            if(r3==0){
-                return "Fizz";
+        int n = (i/3);
+        int m = (i/5);
+            if (i == n && i == m){
+                System.out.print("FizzBuzz");
             }else{
-                if (r5 == 0){
-                    return"buzz";
+                if (i==n){
+                    System.out.print("Fizz");
+                }else{
+                    if(i==m){
+                        System.out.print("Buzz");
+                    }else{
+                       System.out.println(i);
                 }
             }
         }
-        return Integer.toString(n);
+    }
 }
 }
