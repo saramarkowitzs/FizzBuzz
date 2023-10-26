fc44db5735a85f331e2629fef19ff5a194f6003f
Sara Markowitz <sara.markowitz@mrjaffesclass.com>
2023-10-26 11:12:54 -0700

    AWEF

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,9 +1,9 @@
 public class FizzBuzz{
 public static void main(String[] args) {
         for (int i=1; i<=100; i++){
-        int f = (i/3);
-        int b = (i/5);
-            if (f == 0 && b==0){
+        int f = i%3;
+        int b = i%5;
+            if ( f == 0 && b == 0){
                 System.out.print("FizzBuzz");
             }else{
                 if (f==0){
